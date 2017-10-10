b3687fd6f96925e35bece507d665a4ef4cf7bec9
ojab <ojab@ojab.ru>
2017-10-10 22:48:29 +0000

    Hack up saving of name from data-uri

--- lib/carrierwave_data_uri/parser.rb
+++ lib/carrierwave_data_uri/parser.rb
@@ -5,11 +5,25 @@ module CarrierWave
     class Parser
       attr_reader :type, :encoder, :data, :extension
 
+      DATA_REGEXP          = /data:/
+      MEDIA_TYPE_REGEXP    = /[-\w.+]+\/[-\w.+]+(;[-\w.+]+=[^;,]+)*/
+      BASE64_REGEXP        = /;base64/
+      CONTENT_SEPARATOR    = /,/
+      DEFAULT_CONTENT_TYPE = "text/plain"
+
       def initialize(data_uri)
-        uri = URI::Data.new data_uri
-        @type = uri.content_type
+        scanner = StringScanner.new(data_uri)
+        scanner.scan(DATA_REGEXP) or raise ParseError, "data URI has invalid format"
+        media_type = scanner.scan(MEDIA_TYPE_REGEXP)
+        base64 = scanner.scan(BASE64_REGEXP)
+        scanner.scan(CONTENT_SEPARATOR) or raise ParseError, "data URI has invalid format"
+        content_type = media_type[/^[^;]+/] if media_type
+        params = media_type.split(';')[1..-1].map { |param| param.split('=') }.to_h
+
+        @type = content_type
         @extension = @type.split('/')[1]
-        @data = uri.data
+        @data = Base64.decode64(scanner.post_match)
+        @original_filename = params['name']
       rescue URI::InvalidURIError
         raise InvalidData, 'Cannot parse data'
       end
@@ -24,8 +38,8 @@ module CarrierWave
           file.binmode
           file << binary_data
           file.rewind
-          file.original_filename = options[:original_filename]
-          file.content_type = options[:content_type]
+          file.original_filename = options[:original_filename] || @original_filename
+          file.content_type = options[:content_type] || @type
           file
         end
       end
